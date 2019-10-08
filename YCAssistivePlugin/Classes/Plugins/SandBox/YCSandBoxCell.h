5a9057aa16c2b296e985fd6f34999165fd577f0a
沈伟航 <809827782@qq.com>
2019-10-08 17:40:02 +0800

    update

---
+++ YCAssistivePlugin/Classes/Plugins/SandBox/YCSandBoxCell.h
@@ -0,0 +1,17 @@
+//
+//  YCSandBoxCell.h
+//  YCAssistivePlugin
+//
+//  Created by shenweihang on 2019/10/8.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@class YCSandBoxModel;
+@interface YCSandBoxCell : UITableViewCell
+
+- (void)renderUIWithModel:(YCSandBoxModel *)model;
+@end
+
+NS_ASSUME_NONNULL_END
--- YCAssistivePlugin/Classes/Plugins/SandBox/YCSandBoxViewController.m
+++ YCAssistivePlugin/Classes/Plugins/SandBox/YCSandBoxViewController.m
@@ -9,6 +9,7 @@
 #import <Masonry/Masonry.h>
 #import "YCSandBoxModel.h"
 #import "YCAssistiveDefine.h"
+#import "YCSandBoxCell.h"
 
 @interface YCSandBoxViewController ()<UITableViewDelegate,UITableViewDataSource>
 
@@ -50,10 +51,53 @@
 }
 
 
-- (void)fetchFiles:(NSString *)filePath {
+- (void)fetchFilesByPath:(NSString *)filePath {
     
+    YCSandBoxModel *sandBoxModel = [[YCSandBoxModel alloc] init];
+    if (!filePath || [filePath isEqualToString:self.rootPath]) {
+        filePath = self.rootPath;
+    }
     NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error = nil;
+    NSArray *paths = [fileManager contentsOfDirectoryAtPath:filePath error:&error];
+    for (NSString *path in paths) {
+        BOOL isDir = NO;
+        NSString *fullPath = [filePath stringByAppendingPathComponent:path];
+        [fileManager fileExistsAtPath:fullPath isDirectory:&isDir];
+        YCSandBoxModel *model = [[YCSandBoxModel alloc] init];
+        if (isDir) {
+            model.fileType = YCSandBoxFileTypeDirectory;
+        }else {
+            model.fileType = YCSandBoxFileTypeFile;
+        }
+        model.name = path;
+        model.path = fullPath;
+        [self.fileList addObject:model];
+    }
+    [self.tableView reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.fileList.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60.0f;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    YCSandBoxCell *cell = [tableView dequeueReusableCellWithIdentifier:@"YCSandBoxCell"];
+    if (!cell) {
+        cell = [[YCSandBoxCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"YCSandBoxCell"];
+    }
+    [cell renderUIWithModel:self.fileList[indexPath.row]];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 #pragma mark - getter
