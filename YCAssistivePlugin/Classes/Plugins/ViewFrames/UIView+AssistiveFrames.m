bb71ff0f5067fb2b02a88a196dfff5b2cf665a0c
沈伟航 <809827782@qq.com>
2019-10-11 15:11:33 +0800

    fix

--- YCAssistivePlugin/Classes/Plugins/ViewFrames/UIView+AssistiveFrames.m
+++ YCAssistivePlugin/Classes/Plugins/ViewFrames/UIView+AssistiveFrames.m
@@ -58,6 +58,7 @@
         if (self.frameLayer) {
             self.frameLayer.hidden = YES;
             [self.frameLayer removeFromSuperlayer];
+            self.frameLayer = nil;
         }
     }
 }
